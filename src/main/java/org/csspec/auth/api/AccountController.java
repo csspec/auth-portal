4e17c0155d8ec6d2cd378a47417366fed29d5349
Prince Dhaliwal <dhaliwalprince@hotmail.com>
2016-10-11 21:13:42 +0530

    [api] Validating requests on basis of user role

--- src/main/java/org/csspec/auth/api/AccountController.java
+++ src/main/java/org/csspec/auth/api/AccountController.java
@@ -1,7 +1,9 @@
 package org.csspec.auth.api;
 
+import org.apache.catalina.servlet4preview.http.HttpServletRequest;
 import org.csspec.auth.db.repositories.UserAccountRepository;
 import org.csspec.auth.db.schema.Account;
+import org.csspec.auth.db.schema.UserRole;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -12,24 +14,31 @@ import java.util.List;
 public class AccountController {
     private UserAccountRepository repository;
 
+    private RequestApproval requestApproval;
+
     @Autowired
-    AccountController(UserAccountRepository repository) {
+    AccountController(UserAccountRepository repository, RequestApproval requestApproval) {
         this.repository = repository;
+        this.requestApproval = requestApproval;
     }
 
     @RequestMapping(method = RequestMethod.GET)
-    public List<Account> getAllAccounts() {
+    public List<Account> getAllAccounts(HttpServletRequest request) throws Exception {
+        requestApproval.approveRequest(request, UserRole.TEACHER);
         return repository.findAll();
     }
 
     @RequestMapping(method = RequestMethod.POST)
-    public Account createAccount(@RequestBody Account account) {
+    public Account createAccount(@RequestBody Account account, HttpServletRequest request) throws Exception {
+        requestApproval.approveRequest(request, UserRole.ADMIN);
         repository.save(account);
         return account;
     }
 
     @RequestMapping(value = "/{accountId}", method = RequestMethod.GET)
-    public Account getAccount(@PathVariable String accountId) {
+    public Account getAccount(@PathVariable String accountId, HttpServletRequest request) throws Exception {
+        requestApproval.approveRequest(request, UserRole.ADMIN);
         return repository.findUserById(accountId);
     }
+
 }
--- src/main/java/org/csspec/auth/api/RequestApproval.java
+++ src/main/java/org/csspec/auth/api/RequestApproval.java
@@ -4,15 +4,22 @@ import org.apache.catalina.servlet4preview.http.HttpServletRequest;
 import org.csspec.auth.Configuration;
 import org.csspec.auth.config.JwtConfiguration;
 import org.csspec.auth.db.schema.Account;
+import org.csspec.auth.db.schema.ClientApplication;
 import org.csspec.auth.db.schema.UserRole;
 import org.csspec.auth.db.services.AuthUserService;
 import org.csspec.auth.db.services.ClientApplicationDetailService;
+import org.csspec.auth.exceptions.InsufficientAuthorizationException;
 import org.csspec.auth.exceptions.InsufficientRoleException;
+import org.csspec.auth.exceptions.InvalidAuthorizationTokenException;
+import org.csspec.auth.exceptions.UnknownClientException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.web.authentication.rememberme.InvalidCookieException;
 import org.springframework.stereotype.Component;
 
 import javax.servlet.http.Cookie;
+import java.util.Collection;
+import java.util.Enumeration;
+import java.util.Map;
 
 /**
  * request approving routines
@@ -62,4 +69,61 @@ public class RequestApproval {
         }
         return account;
     }
+
+
+    private String parseAuthorizationHeader(HttpServletRequest request) throws Exception {
+        Enumeration<String> headers = request.getHeaders("Authorization");
+        if (headers == null) {
+            throw new InsufficientAuthorizationException();
+        }
+        String authToken = headers.hasMoreElements() ? headers.nextElement() : null;
+
+        if (authToken == null)
+            throw new InsufficientAuthorizationException();
+
+        if (!authToken.trim().startsWith("Bearer "))
+            return null;
+
+        String []strings = authToken.split(" ");
+        if (strings.length <= 1)
+            return null;
+
+        return strings[1];
+    }
+
+    /**
+     * validate and return the authorized services
+     * @param servicesAuthorized actual authorized services
+     * @param referringClient client which is requesting for services
+     * @return space separated list of services
+     */
+    public String validateServices(String servicesAuthorized, ClientApplication referringClient) {
+        Collection<String> accessibleServices = referringClient.getAllowedServices();
+        String []authorizedServices = servicesAuthorized.split(" ");
+
+        // TODO: add logic for validating the service
+        return servicesAuthorized;
+    }
+
+    public void approveRequestFromClient(HttpServletRequest request, ClientApplication application) throws Exception {
+        String authorizationHeader = parseAuthorizationHeader(request);
+        if (authorizationHeader == null || authorizationHeader.length() == 0) {
+            throw new InsufficientAuthorizationException();
+        }
+
+        Map<String, Object> claims = JwtConfiguration.getClaims(authorizationHeader);
+        String clientId = (String)claims.get("sub");
+        if (clientId == null) {
+            throw new InvalidAuthorizationTokenException();
+        }
+
+        ClientApplication clientApplication = clientDetailsService.loadClientByClientId(clientId);
+        if (clientApplication == null || !application.equals(clientApplication))
+            throw new UnknownClientException(clientId);
+
+        String services = (String)claims.get("scope");
+        if (services != null)
+            validateServices(services, application);
+    }
+
 }
